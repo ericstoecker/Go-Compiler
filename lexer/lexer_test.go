46ec150c0bd6b743d6cd437a74c2968e1007173e
Eric <eric.stoecker@gmx.net>
2024-04-26 17:15:26 +0200

    refactor: Remove second test

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,12 +1,12 @@
 package lexer
 
 import (
-    "testing"
-    "example/hello/token"
+	"example/hello/token"
+	"testing"
 )
 
 func TestNextToken(t *testing.T) {
-    input := `let five = 5;
+	input := `let five = 5;
     let ten = 10;
 
     let add = fn(x, y) {
@@ -14,83 +14,67 @@ func TestNextToken(t *testing.T) {
     };
 
     let result = add(five, ten);
+
+    ==
+    !=
     `
 
-    tests := []struct {
-        expectedType token.TokenType
-        expectedLiteral string
-    } {
-        {token.LET, "let"},
-        {token.IDENT, "five"},
-        {token.ASSIGN, "="},
-        {token.INT, "5"},
-        {token.SEMICOLON, ";"},
-        {token.LET, "let"},
-        {token.IDENT, "ten"},
-        {token.ASSIGN, "="},
-        {token.INT, "10"},
-        {token.SEMICOLON, ";"},
-        {token.LET, "let"},
-        {token.IDENT, "add"},
-        {token.ASSIGN, "="},
-        {token.FUNCTION, "fn"},
-        {token.LPAREN, "("},
-        {token.IDENT, "x"},
-        {token.COMMA, ","},
-        {token.IDENT, "y"},
-        {token.RPAREN, ")"},
-        {token.LBRACE, "{"},
-        {token.IDENT, "x"},
-        {token.PLUS, "+"},
-        {token.IDENT, "y"},
-        {token.SEMICOLON, ";"},
-        {token.RBRACE, "}"},
-        {token.SEMICOLON, ";"},
-        {token.LET, "let"},
-        {token.IDENT, "result"},
-        {token.ASSIGN, "="},
-        {token.IDENT, "add"},
-        {token.LPAREN, "("},
-        {token.IDENT, "five"},
-        {token.COMMA, ","},
-        {token.IDENT, "ten"},
-        {token.RPAREN, ")"},
-        {token.SEMICOLON, ";"},
-        {token.EOF, ""},
-    }
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
+		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.EQUALS, "=="},
+		{token.NOT_EQUALS, "!="},
+		{token.EOF, ""},
+	}
 
-    l := New(input)
+	l := New(input)
 
-    for i, tt := range tests {
-        tok := l.NextToken()
+	for i, tt := range tests {
+		tok := l.NextToken()
 
-        if tok.Type != tt.expectedType {
-            t.Fatalf("tests[%d] - type wrong. expected=%q, got %q", i, tt.expectedType, tok.Type)
-        }
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - type wrong. expected=%q, got %q", i, tt.expectedType, tok.Type)
+		}
 
-        if tok.Literal != tt.expectedLiteral {
-            t.Fatalf("tests[%d] - literal wrong. expected=%q, got %q", i, tt.expectedLiteral, tok.Literal)
-        }
-    }
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got %q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
 }
-
-func TestDoubleTokens(t *testing.T) {
-    equals := `==`
-
-    l := New(equals)
-    tok := l.NextToken()
-
-    if tok.Type != token.EQUALS {
-        t.Fatalf("Expected '==', got %q", tok.Type)
-    }
-
-    not_equals := `!=`
-
-    notEqualsLexer := New(not_equals)
-    readToken := notEqualsLexer.NextToken()
-
-    if readToken.Type != token.NOT_EQUALS {
-        t.Fatalf("Expected '!=', got %q", tok.Type)
-    }
-}
-
