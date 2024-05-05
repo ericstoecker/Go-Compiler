7c20af73e6e4d375f141ce2a8f557e4fdc6c0de5
Eric <eric.stoecker@gmx.net>
2024-05-05 16:06:34 +0200

    feat: Evaluate functions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -28,13 +28,12 @@ func (eval *Evaluator) evaluateStatement(stmt ast.Statement) Object {
 	switch v := stmt.(type) {
 	case *ast.ExpressionStatement:
 		return eval.evaluateExpression(v.Expression)
-	case *ast.ReturnStatement:
-		return nil
 	case *ast.LetStatement:
 		value := eval.evaluateExpression(v.Value)
-		// add result to environment
 		eval.environment[v.Name.Value] = value
 		return nil
+	case *ast.ReturnStatement:
+		return &ReturnObject{ReturnValue: eval.evaluateExpression(v.ReturnValue)}
 	default:
 		return nil
 	}
@@ -50,9 +49,41 @@ func (eval *Evaluator) evaluateExpression(expression ast.Expression) Object {
 		return &IntegerObject{Value: v.Value}
 	case *ast.Identifier:
 		return eval.environment[v.Value]
+	case *ast.FunctionLiteral:
+		params := make([]string, 0)
+		for _, param := range v.Parameters {
+			params = append(params, param.Value)
+		}
+		return &FunctionObject{Parameters: params, Body: v.Body}
+	case *ast.CallExpression:
+		functionObj, ok := eval.environment[v.TokenLiteral()].(*FunctionObject)
+		if !ok {
+			return nil
+		}
+
+		for i, param := range functionObj.Parameters {
+			eval.environment[param] = eval.evaluateExpression(v.Arguments[i])
+		}
+
+		return eval.evaluateBlockStatement(functionObj.Body)
 	default:
 		return nil
 	}
+}
+
+func (eval *Evaluator) evaluateBlockStatement(blockStmt *ast.BlockStatement) Object {
+	var result Object
+
+	for _, stmt := range blockStmt.Statements {
+		result = eval.evaluateStatement(stmt)
+
+		returnStmt, ok := result.(*ReturnObject)
+		if ok {
+			return returnStmt.ReturnValue
+		}
+	}
+
+	return result
 }
 
 func (eval *Evaluator) evaluatePrefixExpression(prefixExpr *ast.PrefixExpression) Object {
@@ -96,13 +127,24 @@ func (eval *Evaluator) evaluateInfixExpression(infixExpr *ast.InfixExpression) O
 }
 
 type Object interface {
-	String() string
+	object()
 }
 
 type IntegerObject struct {
 	Value int64
 }
 
-func (intObj *IntegerObject) String() string {
-	return "10"
+func (intObj *IntegerObject) object() {}
+
+type FunctionObject struct {
+	Parameters []string
+	Body       *ast.BlockStatement
 }
+
+func (funcObj *FunctionObject) object() {}
+
+type ReturnObject struct {
+	ReturnValue Object
+}
+
+func (returnObj *ReturnObject) object() {}
