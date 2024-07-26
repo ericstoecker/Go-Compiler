575a43c7555beb212bb18aaffab3b78d76746b73
Eric <eric.stoecker@gmx.net>
2024-07-26 15:38:04 +0200

    feat: Add basic table driven scanner

---
+++ scanner-generator/table-driven-scanner.go
@@ -0,0 +1,84 @@
+package scannergenerator
+
+import (
+	"compiler/token"
+	"slices"
+)
+
+type TableDrivenScanner struct {
+	input        string
+	position     int
+	readPosition int
+	ch           byte
+
+	dfa *Dfa
+}
+
+func New(input string, dfa *Dfa) *TableDrivenScanner {
+	s := &TableDrivenScanner{input: input, dfa: dfa}
+	s.readChar()
+	return s
+}
+
+func (s *TableDrivenScanner) NextToken() token.Token {
+	s.skipWhitespace()
+
+	state := 0
+	lexeme := ""
+	stack := []int{}
+
+	for state != -1 {
+		stack = append(stack, state)
+		lexeme += string(s.ch)
+
+		if stateAfterTransition, ok := s.dfa.Transitions[string(s.ch)][state]; ok {
+			state = stateAfterTransition
+		} else {
+			state = -1
+		}
+		s.readChar()
+	}
+
+	for !s.isAcceptingState(state) && len(stack) > 1 {
+		state = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		lexeme = lexeme[:len(lexeme)-1]
+		s.rollback()
+	}
+
+	if s.isAcceptingState(state) {
+		return token.Token{Type: "ACCEPT", Literal: lexeme}
+	}
+	return token.Token{Type: token.ILLEGAL, Literal: lexeme}
+}
+
+func (s *TableDrivenScanner) isAcceptingState(state int) bool {
+	return slices.Contains(s.dfa.AcceptingStates, state)
+}
+
+func (s *TableDrivenScanner) readChar() {
+	if s.readPosition >= len(s.input) {
+		s.ch = 0
+		return
+	}
+
+	s.ch = s.input[s.readPosition]
+	s.position = s.readPosition
+	s.readPosition += 1
+}
+
+func (s *TableDrivenScanner) rollback() {
+	if s.readPosition == 0 {
+		return
+	}
+
+	s.readPosition = s.position
+	s.position -= 1
+	s.ch = s.input[s.readPosition]
+}
+
+func (s *TableDrivenScanner) skipWhitespace() {
+	for s.ch == ' ' || s.ch == '\r' || s.ch == '\n' || s.ch == '\t' {
+		s.readChar()
+	}
+}
---
+++ scanner-generator/table-driven-scanner_test.go
@@ -0,0 +1,49 @@
+package scannergenerator
+
+import (
+	"compiler/token"
+	"testing"
+)
+
+func TestAccepting(t *testing.T) {
+	RegexpToNfaConverter := NewRegexpToNfaConverter("a(b|c)*")
+	nfa := RegexpToNfaConverter.Convert()
+
+	nfaToDfaConverter := NewNfaToDfaConverter(nfa)
+	dfa := nfaToDfaConverter.Convert()
+
+	tests := []struct {
+		input         string
+		expectedToken token.Token
+	}{
+		{
+			"a",
+			token.Token{Type: "ACCEPT", Literal: "a"},
+		},
+		{
+			"ab",
+			token.Token{Type: "ACCEPT", Literal: "ab"},
+		},
+		{
+			"ac",
+			token.Token{Type: "ACCEPT", Literal: "ac"},
+		},
+		{
+			"abb",
+			token.Token{Type: "ACCEPT", Literal: "abb"},
+		},
+		{
+			"d",
+			token.Token{Type: token.ILLEGAL, Literal: "d"},
+		},
+	}
+
+	for _, tt := range tests {
+		scanner := New(tt.input, dfa)
+		token := scanner.NextToken()
+		if token != tt.expectedToken {
+			t.Logf("current input: %s", tt.input)
+			t.Errorf("expected: %v, got: %v", tt.expectedToken, token)
+		}
+	}
+}
